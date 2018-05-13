cb6f1ce00c6eaa618377f7c9e993deca521952a1
Falk Rismansanj <info@zitrotec.de>
2018-05-14 01:50:59 +0200

    * Fehlerbehebungen im Katalog

--- blista/EighthViewController.swift
+++ blista/EighthViewController.swift
@@ -15,12 +15,12 @@ class EighthViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var webView8: UIWebView!
     @IBOutlet weak var activity: UIActivityIndicatorView!
-    
+            
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         let reachability = Reachability()!
         
         switch reachability.connection {
@@ -109,10 +109,77 @@ class EighthViewController: UIViewController, UIWebViewDelegate {
     
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         if navigationType == UIWebViewNavigationType.linkClicked {
-            
+
             if (request.url!.host == "katalog.blista.de") {
                 return true
             } else {
+
+                let userDefaults = UserDefaults.standard
+                let wifisetting = userDefaults.bool(forKey: "wifionly")
+                if wifisetting == true
+                {
+                    
+                    let reachability = Reachability()!
+                    
+                    if reachability.connection == .cellular {
+                        
+                        if (request.url!.host == "www.blista.de") {
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(request.url!, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(request.url!)
+                            }
+                            return false
+                        }
+                        
+                        if (request.url!.scheme == "mailto") {
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(request.url!, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(request.url!)
+                            }
+                            return false
+                        }
+                        
+                        if (request.url!.scheme == "tel") {
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(request.url!, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(request.url!)
+                            }
+                            return false
+                        }
+                        
+                        if self.webView8.isLoading {
+                        self.webView8.stopLoading()
+                        }
+                        
+                        let wifialert =  UIAlertController(title: "Keine WLAN-Verbindung", message: "Du hast in den Einstellungen festgelegt, dass du Downloads nur über WLAN zulässt. Um dies zu ändern gehe in die Einstellungen.", preferredStyle: .alert)
+                        
+                        
+                        let cancelAction = UIAlertAction(title: "OK", style: .cancel) {action -> Void in
+
+                        }
+                        wifialert.addAction(cancelAction)
+                        
+                        let okAction = UIAlertAction(title: "Einstellungen", style: .default) {action -> Void in
+                            
+                            let url = URL(string:UIApplicationOpenSettingsURLString)
+                            
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                            } else {
+                                UIApplication.shared.openURL(url!)
+                            }
+                        }
+                        wifialert.addAction(okAction)
+                        
+                        self.present(wifialert, animated: true, completion: nil)
+                            
+                        return true
+                        
+                    }
+                }
                 
                 if #available(iOS 10.0, *) {
                     UIApplication.shared.open(request.url!, options: [:], completionHandler: nil)
@@ -125,11 +192,10 @@ class EighthViewController: UIViewController, UIWebViewDelegate {
         }
         return true
     }
-        
+            
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
 }
